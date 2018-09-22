8ddabc5f73bd7404d32ac43e42384212f2409006
minhdao <daominh91197@gmail.com>
2018-09-22 21:18:09 +0300

    submit form

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -4,7 +4,6 @@ import { FETCH_POSTS } from "../actions";
 export default function(state = {}, action) {
   switch (action.type) {
     case FETCH_POSTS:
-      console.log(action.payload.data);
       return _.mapKeys(action.payload.data, "id");
     default:
       return state;
